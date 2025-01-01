44e1ec5e7a00cdb6df8fd30b951cc2c7f024770f
lmx <14036563+lmx12330@user.noreply.gitee.com>
2025-01-01 02:38:14 +0000

    update resource/install.ps1.
    Signed-off-by: lmx <14036563+lmx12330@user.noreply.gitee.com>

--- resource/install.ps1
+++ resource/install.ps1
@@ -5,7 +5,7 @@ New-Item -Path $tempFolderPath -ItemType Directory -Force
 
 $result = dotnet --list-runtimes | Select-String "9.0"
 if (!$result)
-{Write-Host "正在下载.NET 9.0 运行时,请勿退出"
+{
     $installProgramPath = Join-Path -Path $tempFolderPath -ChildPath '.NET_9.0_RUNTIME.exe'
 if ([Environment]::Is64BitOperatingSystem) $downloadurl = "https://download.visualstudio.microsoft.com/download/pr/685792b6-4827-4dca-a971-bce5d7905170/1bf61b02151bc56e763dc711e45f0e1e/windowsdesktop-runtime-9.0.0-win-x64.exe"
 else $downloadurl = "https://download.visualstudio.microsoft.com/download/pr/8dfbde7b-c316-418d-934a-d3246253f342/69c6a35b77a4f01b95588e1df2bddf9a/windowsdesktop-runtime-9.0.0-win-x86.exe"
@@ -13,12 +13,11 @@ else $downloadurl = "https://download.visualstudio.microsoft.com/download/pr/8df
     $webClient.DownloadFile($downloadurl, $installProgramPath)
     Start-Process $installProgramPath -Wait}
 
-Write-Host "正在下载程序压缩包,请勿退出"
 $downloadurl = "https://gitee.com/lmx12330/window-op/raw/master/resource/windowOP.zip"
 $zipPath = Join-Path -Path $tempFolderPath -ChildPath "windowOP.zip"
 $targetPath = Join-Path -Path $appDataPath -ChildPath "windowOP"
 $exePath = Join-Path -Path $targetPath -ChildPath "windowOP.exe"
-$webClient.DownloadFile($downloadurl, $zipPath)  # 使用同步下载
+$webClient.DownloadFile($downloadurl, $zipPath) 
 Expand-Archive -Path $zipPath -DestinationPath $targetPath -Force
 Start-Process $exePath
 Remove-Item -Path $tempFolderPath -Recurse -Force
